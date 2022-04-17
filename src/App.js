0484f5462c1b08f6e159ab7fa1d34caaf2dfbbf1
Abhinav Rai <raiabhinavrai1994@gmail.com>
2022-04-17 22:21:46 +0530

    useFetch and infinite scroller

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-import { useEffect, useState } from "react";
-import { getData, TrendingApi } from "./apis";
+import { useCallback, useEffect, useRef, useState } from "react";
+// import { getData, TrendingApi } from "./apis";
 import "./styles.css";
+import useFetch from "./useFetch";
 
 export default function App() {
-  const [giphyData, updateGiphyData] = useState([]);
-  const [paginationData, updatePaginationData] = useState({});
-  useEffect(() => {
-    getData(TrendingApi).then((res) => {
-      const { data, pagination } = res;
-      updateGiphyData([...giphyData, ...data]);
-      updatePaginationData(pagination);
-    });
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const { loading, error, list } = useFetch(query, page);
+  const loader = useRef(null);
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setPage((prev) => prev + 1);
+    }
   }, []);
+
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 0
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (loader.current) observer.observe(loader.current);
+  }, [handleObserver]);
+  console.log(list);
   return (
-    <div className="App">
+    <div className="App" id="root">
       <h3>Trending Giphy</h3>
+      <input type="text" value={query} onChange={handleChange} />
       <div className="photo-container">
-        {giphyData.map((d) => {
-          return (
-            <div key={d.title}>
-              <img src={d.images.preview_webp.url} alt={d.title} />
-            </div>
-          );
-        })}
+        {/* {list.map((item) => (
+          <div key={item.title}>
+            <img src={item.images.preview_webp.url} alt={item.title} />
+          </div>
+        ))} */}
       </div>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error!</p>}
+      <div ref={loader} />
     </div>
   );
 }
--- src/apis.js
+++ src/apis.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const API_KEY = "x6sV5kNZAZP6YXIQSzMZm4e16eLrokbJ";
+const basePoint = "https://api.giphy.com/v1/gifs";
+const commonParams = `api_key=${API_KEY}&limit=25&rating=g`;
 
-export const TrendingApi =
-  "https://api.giphy.com/v1/gifs/trending?api_key=x6sV5kNZAZP6YXIQSzMZm4e16eLrokbJ&limit=25&rating=g";
+export const TrendingApi = `${basePoint}/trending?${commonParams}`;
+export const SearchApi = `${basePoint}/search?${commonParams}`;
 
 export function getData(url) {
   return axios
